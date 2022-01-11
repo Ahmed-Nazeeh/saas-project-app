166886c3820e69f81809e92954a9e59f590dfffb
Ahmed-Nazeeh <89453140+Ahmed-Nazeeh@users.noreply.github.com>
2022-01-11 17:57:32 +0200

    add rails 12factor gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :confirmable, :trackable,
          :recoverable, :rememberable, :validatable
 
-  #acts_as_universal_and_determines_account
+  acts_as_universal_and_determines_account
   has_one :member, :dependent => :destroy
 
 end
