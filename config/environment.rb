97e91ccbb579adb4454edf48e54f1280ce730613
Ahmed-Nazeeh <89453140+Ahmed-Nazeeh@users.noreply.github.com>
2022-01-11 15:44:00 +0200

    heroku deploy final

--- config/environment.rb
+++ config/environment.rb
@@ -5,12 +5,3 @@ require_relative "application"
 Rails.application.initialize!
 
 
-ActionMailer::Base.smtp_settings = {
-  :user_name => ENV['SENDGRID_USERNAME'],
-  :password => ENV['SENDGRID_PASSWORD'],    
-  :domain => 'ahmed-saas-app.herokuapp.com',
-  :address => 'smtp.sendgrid.net',
-  :port => '587',
-  :authentication => :plain,
-  :enable_starttls_auto => true
-}
